b60101ef3e88e7aba64f681c9771824848cef75d
singuneet <guneetsingh209@gmail.com>
2025-04-04 17:59:54 +0530

    [any message]

--- src/server.cpp
+++ src/server.cpp
@@ -9,8 +9,38 @@
 #include <netdb.h>
 #include <thread>
 #include <fstream>
+#include <sstream>
+#include <zlib.h>
 
 std::string directory;
+
+std::string gzip_compress(const std::string& data) {
+    z_stream zs;
+    memset(&zs, 0, sizeof(zs));
+    
+    if (deflateInit2(&zs, Z_BEST_COMPRESSION, Z_DEFLATED, 15 | 16, 8, Z_DEFAULT_STRATEGY) != Z_OK) {
+        return "";
+    }
+    
+    zs.next_in = (Bytef*)data.data();
+    zs.avail_in = data.size();
+    
+    int ret;
+    char outbuffer[32768];
+    std::string outstring;
+    
+    do {
+        zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+        zs.avail_out = sizeof(outbuffer);
+        ret = deflate(&zs, Z_FINISH);
+        if (outstring.size() < zs.total_out) {
+            outstring.append(outbuffer, zs.total_out - outstring.size());
+        }
+    } while (ret == Z_OK);
+    
+    deflateEnd(&zs);
+    return (ret == Z_STREAM_END) ? outstring : "";
+}
 
 void handle_client(int client_fd) {
   char buffer[1024] = {0};
@@ -31,19 +61,26 @@ void handle_client(int client_fd) {
     response = "HTTP/1.1 200 OK\r\n\r\n";
   } else if (path.rfind("/echo/", 0) == 0) {
     std::string echo_content = path.substr(6);
-    std::string headers = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(echo_content.length()) + "\r\n";
+    std::string headers = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n";
     
-    // CHANGED: Improved parsing of Accept-Encoding to handle multiple encodings
     size_t enc_start = request.find("Accept-Encoding:");
+    bool gzip_supported = false;
     if (enc_start != std::string::npos) {
       size_t enc_end = request.find("\r\n", enc_start);
       std::string encodings = request.substr(enc_start + 16, enc_end - enc_start - 16);
       if (encodings.find("gzip") != std::string::npos) {
-        headers += "Content-Encoding: gzip\r\n";
+        gzip_supported = true;
       }
     }
     
-    response = headers + "\r\n" + echo_content;
+    if (gzip_supported) {
+      std::string compressed = gzip_compress(echo_content);
+      headers += "Content-Encoding: gzip\r\nContent-Length: " + std::to_string(compressed.length()) + "\r\n\r\n";
+      response = headers + compressed;
+    } else {
+      headers += "Content-Length: " + std::to_string(echo_content.length()) + "\r\n\r\n";
+      response = headers + echo_content;
+    }
   } else if (path == "/user-agent") {
     size_t ua_start = request.find("User-Agent: ");
     if (ua_start != std::string::npos) {
@@ -54,9 +91,9 @@ void handle_client(int client_fd) {
     } else {
       response = "HTTP/1.1 400 Bad Request\r\n\r\n";
     }
-  } else if (path.rfind("/files/", 0) == 0) { // CHANGED: Added support for serving and storing files
+  } else if (path.rfind("/files/", 0) == 0) {
     std::string filename = path.substr(7);
-    if (request.rfind("POST", 0) == 0) { // CHANGED: Handle POST requests
+    if (request.rfind("POST", 0) == 0) {
       size_t content_length_pos = request.find("Content-Length: ");
       if (content_length_pos != std::string::npos) {
         content_length_pos += 16;
@@ -87,61 +124,25 @@ void handle_client(int client_fd) {
   }
 
   send(client_fd, response.c_str(), response.length(), 0);
-  std::cout << "Response sent: " << response;
-
   close(client_fd);
 }
 
 int main(int argc, char **argv) {
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  
-  std::cout << "Logs from your program will appear here!\n";
-
   if (argc == 3 && std::string(argv[1]) == "--directory") {
-    directory = argv[2]; // CHANGED: Store directory argument
+    directory = argv[2];
   }
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
-  }
+  if (server_fd < 0) return 1;
 
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(4221);
-
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 4221\n";
-    return 1;
-  }
-
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-
-  std::cout << "Waiting for clients to connect...\n";
+  struct sockaddr_in server_addr = {AF_INET, htons(4221), INADDR_ANY};
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) return 1;
+  if (listen(server_fd, 5) != 0) return 1;
 
   while (true) {
     struct sockaddr_in client_addr;
-    int client_addr_len = sizeof(client_addr);
-    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-    if (client_fd < 0) {
-      std::cerr << "Client connection failed\n";
-      continue;
-    }
-    std::cout << "Client connected\n";
-    std::thread(handle_client, client_fd).detach();
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, nullptr);
+    if (client_fd >= 0) std::thread(handle_client, client_fd).detach();
   }
 
   close(server_fd);
